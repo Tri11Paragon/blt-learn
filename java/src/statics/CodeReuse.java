fda17cbb8a31b2d8d1da852fc8c6efa82e87cf41
Brett Laptop <tri11paragon@tpgc.me>
2025-10-29 13:38:33 -0400

    stage

---
+++ java/src/statics/CodeReuse.java
@@ -0,0 +1,21 @@
+package statics;
+
+import brock.*;
+
+public class CodeReuse {
+
+
+
+    public class Yertle {
+        public Yertle() {
+
+        }
+    }
+
+    public class Main {
+        public static void main(String[] args){
+
+        }
+    }
+
+}
