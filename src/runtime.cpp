bfde1905975e2d81d02e946266477f9fc63553fd
Brett Laptop <tri11paragon@tpgc.me>
2025-11-29 21:53:25 -0500

    lots of code

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -15,3 +15,60 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+#include <blt/gfx/window.h>
+#include "blt/gfx/renderer/resource_manager.h"
+#include "blt/gfx/renderer/batch_2d_renderer.h"
+#include "blt/gfx/renderer/camera.h"
+#include <imgui.h>
+#include <filesystem>
+
+blt::gfx::matrix_state_manager global_matrices;
+blt::gfx::resource_manager     resources;
+blt::gfx::batch_renderer_2d    renderer_2d(resources, global_matrices);
+blt::gfx::first_person_camera  camera;
+
+void invoke_runtime(const std::filesystem::path& exec)
+{
+
+}
+
+void init(const blt::gfx::window_data&)
+{
+	using namespace blt::gfx;
+
+
+	global_matrices.create_internals();
+	resources.load_resources();
+	renderer_2d.create();
+}
+
+void update(const blt::gfx::window_data& data)
+{
+	global_matrices.update_perspectives(data.width, data.height, 90, 0.1, 2000);
+
+	camera.update();
+	camera.update_view(global_matrices);
+	global_matrices.update();
+
+	renderer_2d.render(data.width, data.height);
+}
+
+void destroy(const blt::gfx::window_data&)
+{
+	global_matrices.cleanup();
+	resources.cleanup();
+	renderer_2d.cleanup();
+	blt::gfx::cleanup();
+}
+
+int main()
+{
+	#ifdef BLT_AUTORELOAD_RUNTIME
+
+	#else
+		constexpr const char* runtimes[] = {RUNTIMES};
+		BLT_TRACE("{}", runtimes[0]);
+		blt::gfx::init(blt::gfx::window_data{"My Sexy Window", init, update, destroy}.setSyncInterval(1));
+	#endif
+}
